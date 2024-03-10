c60bfa29edfd39bfd5ffca65832b540aa292278a
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2024-03-10 11:33:24 +0530

    update sha256 per brew

--- Formula/command-storage.rb
+++ Formula/command-storage.rb
@@ -5,7 +5,7 @@ class CommandStorage < Formula
   homepage "https://github.com/ashu-tosh-kumar/command-storage"
   url "https://github.com/ashu-tosh-kumar/command-storage/releases/tag/v0.1.0"
   version "0.1.0"
-  sha256 "93defcb5addf53454689e79a8d5a907b5b93550d3185a3d1fb66c00c934036ef"
+  sha256 "2326eeb2bf1a2332b9ff445bb96387e3f845b9d1a369079788fd0f4448385db8"
   license "MIT"
 
   def install
